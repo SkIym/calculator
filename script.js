22c31c9a75977c186ca21b7d573824d6c5b58588
SkIym <abramjosh13@gmail.com>
2023-07-20 10:55:08 +0800

    Add comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // first number --- operator --- second number method 
+
+// Initialize flags
 let fnumExists = false;
 let snumExists = false;
 let opExists = false;
 let inputtingNumber = false;
-
 let input = 1;
 
+// Initialize number, operator, and answer variables
 let num1 = "";
 let num2 = "";
 let op = "";
@@ -20,6 +22,7 @@ let opButtons = Array.from(document.querySelectorAll(".operator"));
 // Arrage number buttons for easier passing of value
 numButtons = numButtons.sort((a, b) => a.value > b.value ? 1 : -1);
 
+// Modifying elements
 const acButton = document.getElementById("clear");
 const delButton = document.getElementById("backspace");
 const calculate = document.getElementById("answer");
@@ -33,9 +36,7 @@ acButton.onclick = () => {
 
 // Backspace
 delButton.onclick = () => {
-    displayedNumber = calcDisplay.innerHTML
-    
-    // first clause improve, it incorrectly resets the display!
+    displayedNumber = calcDisplay.innerHTML;
     
     if (displayedNumber == '0') {
         calcDisplay.innerHTML = 0;
@@ -46,29 +47,38 @@ delButton.onclick = () => {
 
     switch(input) {
 
+        // First number is currently being modified
         case 1:
             let fnumArray = Array.from(num1);
             fnumArray.pop();
             num1 = fnumArray.join("");
             console.log(num1)
+
+            // If the first number is already empty
             if (num1 == "") {
                 inputtingNumber = false;
                 calcDisplay.innerHTML = 0;
                 break;
             }
+
             calcDisplay.innerHTML = num1;
             break;
-
+        
+            // Operator is currently being modified
         case 2:
+
             op = "";
 
+            // Toggle for storeInput()
             opExists = !opExists;
             
             console.log("Curretn asnwer:", tempAnswer)
             inputtingNumber = true;
             input = 1;
+
             break;
 
+        // Second number is currently being modified
         default:
 
             let snumArray = Array.from(num2);
@@ -76,6 +86,7 @@ delButton.onclick = () => {
             num2 = snumArray.join("");
             console.log(num2)
 
+            // If the second number is empty
             if (num2 == "") {
                 calcDisplay.innerHTML = num1;
                 inputtingNumber = false;
@@ -83,13 +94,10 @@ delButton.onclick = () => {
                 input = 2;
                 break;
             }
-            else {
-                calcDisplay.innerHTML = num2;
-            }
 
+            calcDisplay.innerHTML = num2;
             tempAnswer = operate(parseFloat(num1), parseFloat(num2), op);
             console.log("Curretn asnwer:", tempAnswer)
-
             break;
     }
     console.log("input mode", input)
@@ -115,13 +123,15 @@ calcButtons.forEach((btn) => {
         btn.addEventListener("click", displayInput);
     }
     btn.addEventListener("click", storeInput);
+
 });
 
 
 function storeInput(e) {
 
     let n = e.target.value;
-    // If pressed button is a number
+
+    // If the pressed button is a number
     if (numButtons.some(number => n == number.value)) {
         
         // num1 is being inputted
@@ -129,15 +139,16 @@ function storeInput(e) {
 
             num1 += `${n}`;
             console.log("Storing first number", num1);
+
             // If user presses a number after getting an answer
             if (tempAnswer) {
                 tempAnswer = num1;
             }
 
+            // Signal that the first number is being modified
             input = 1;
-
-
         }
+
         // num2 is being inputted
         else {
             num2 += `${n}`;
@@ -148,16 +159,17 @@ function storeInput(e) {
             console.log("Storing second number", num2)
             console.log(tempAnswer, "after second number")
 
+            // Signal that the second number is being modified
             input = 3;
         }
     }
     // If pressed button is an operator
     else if (opButtons.some(op => n == op.value)) {
 
-        // clear first number in the display 
+        // Clear first number in the display 
         inputtingNumber = false;
 
-        // signal next number input
+        // Signal next number input
         opExists = !opExists;
 
         op = n;
@@ -176,11 +188,14 @@ function storeInput(e) {
         
         console.log(tempAnswer, "after operator", op);
 
+        // Signal that the operator is being modified
         input = 2;
         
     }     
 
 }
+
+// Auxiliary functions
 
 function displayInput(e) {
     if (inputtingNumber) {
@@ -226,6 +241,7 @@ function operate(num1, num2, op) {
         return undefined
     }
 
+    // If there is any empty number or operator
     if (isNaN(num1) || isNaN(num2) || op == "")
     {
         return tempAnswer
